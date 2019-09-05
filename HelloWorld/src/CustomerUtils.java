9635b9675eb596e5a366924bc86538f4877dde1a
Trung Vu <wtrungvu@gmail.com>
2019-09-05 18:51:31 +0700

    13 Eclipse Class Header

---
+++ HelloWorld/src/CustomerUtils.java
@@ -0,0 +1,12 @@
+/* =======================================================
+ * (C) Copyright 2019 by Trung Vu. All Rights Reserved.
+ *
+ * @author Asus
+ * @date Sep 5, 2019
+ * @version 1.0
+ *
+ * ======================================================= */
+
+public class CustomerUtils {
+
+}
